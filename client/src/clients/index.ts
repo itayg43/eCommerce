ced785243765c4202eee6dc5f5b64125fd6f6198
itayg43 <93944494+itayg43@users.noreply.github.com>
2022-12-17 20:37:10 +0200

    edit: add sanity api version to sanity client, and env file

--- client/src/clients/index.ts
+++ client/src/clients/index.ts
@@ -1,9 +1,14 @@
 import createClient from '@sanity/client';
 
-import {SANITY_PROJECT_ID, SANITY_PROJECT_DATASET} from '@env';
+import {
+  SANITY_PROJECT_ID,
+  SANITY_PROJECT_DATASET,
+  SANITY_API_VERSION,
+} from '@env';
 
 export const sanityClient = createClient({
   projectId: SANITY_PROJECT_ID,
   dataset: SANITY_PROJECT_DATASET,
+  apiVersion: SANITY_API_VERSION,
   useCdn: SANITY_PROJECT_DATASET === 'production',
 });
--- client/types/env.d.ts
+++ client/types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
   export const SANITY_PROJECT_ID: string;
   export const SANITY_PROJECT_DATASET: string;
+  export const SANITY_API_VERSION: string;
 }
