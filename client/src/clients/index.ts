4502a40d710095774a12123e699c1bfcc752c894
itayg43 <93944494+itayg43@users.noreply.github.com>
2022-12-17 20:31:44 +0200

    edit: clients index file, install groq

--- client/src/clients/index.ts
+++ client/src/clients/index.ts
@@ -1,8 +1,8 @@
-import sanityClient from '@sanity/client';
+import createSanityClient from '@sanity/client';
 
 import {SANITY_PROJECT_ID, SANITY_PROJECT_DATASET} from '@env';
 
-export default sanityClient({
+export const sanityClient = createSanityClient({
   projectId: SANITY_PROJECT_ID,
   dataset: SANITY_PROJECT_DATASET,
   useCdn: SANITY_PROJECT_DATASET === 'production',
