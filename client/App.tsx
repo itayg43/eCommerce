c2af6eba1cc0e5f9968a990eba8dd4ffe22c08af
itayg43 <93944494+itayg43@users.noreply.github.com>
2022-12-19 23:21:21 +0200

    edit: app, and index files

--- client/App.tsx
+++ client/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {Provider as PaperProvider} from 'react-native-paper';
-import {Provider as StoreProvider} from 'react-redux';
 
 import BottomTabNavigator from './src/navigation/BottomTabNavigator';
-import store from './src/redux/store';
 
 const App = () => {
-  return (
-    <StoreProvider store={store}>
-      <PaperProvider>
-        <NavigationContainer>
-          <BottomTabNavigator />
-        </NavigationContainer>
-      </PaperProvider>
-    </StoreProvider>
-  );
+  return <BottomTabNavigator />;
 };
 
 export default App;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,22 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {Provider as StoreProvider} from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import App from './App';
 
-AppRegistry.registerComponent(appName, () => App);
+import store from './src/redux/store';
+
+const Root = () => {
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider>
+        <NavigationContainer>
+          <App />
+        </NavigationContainer>
+      </PaperProvider>
+    </StoreProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Root);
